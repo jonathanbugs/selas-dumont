d65f7c8bff731ddc6ef7820f683b79cfae6fc10e
Jonathan Bugs <jonathanbugs@outlook.com>
2014-03-12 15:58:21 -0300

    inicio produtos

---
+++ produtos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<!--meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"-->
+		<title>Selas Dumont</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<meta property="og:url" content="http://selasdumont.com.br"/>
+		<meta property="og:title" content="Selas Dumont"/>
+		<meta property="og:image" content="http://selasdumont.com.br/img/facebook-thumb.jpg"/>
+		<meta property="og:site_name" content="Selas Dumont"/>
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<link rel="shortcut icon" href="http://selasdumont.com.br/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="http://selasdumont.com.br/apple-touch-icon.png"/>
+
+
+		<link rel="stylesheet" href="css/reset.css"/>
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/produtos.css"/>
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+		
+
+		<!-- Scripts -->
+		<script src="js/vendor/jquery-1.9.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/produto.js"></script>
+	</body>
+</html>
+ 
